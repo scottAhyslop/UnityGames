077c13a37f5917ac7cda65854c41ba35ca46551b
Sammy Cheese <sahyslop@gmail.com>
2016-03-26 17:43:22 -0400

    InitialCommit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level load requeste for: "+name);
+        SceneManager.LoadScene(name);
+        //Application.LoadLevel(name); 
+    }//end LoadLevel
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested for: "+name);
+    }//end QuitRequest
+}
